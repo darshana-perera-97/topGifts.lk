ff405b1184a4d9d73301410c026fa4aec3139672
Darshana Perera <ds.perera.1997@gmail.com>
2025-02-14 21:15:16 +0530

    main banner section added

---
+++ user/src/Design/Layouts/HeroSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="container">
+        <div className="hero-content">
+          <div className="d-flex justify-content-center  ">
+            <h1>Make Your Loved Ones Smile with the Perfect Gift!</h1>
+          </div>
+          <div className="d-flex justify-content-center  ">
+            <h2 className=" py-2">
+              Discover unique and heartfelt gifts for every occasion to Surprise
+              your loved ones with something special.
+            </h2>
+          </div>
+          <button className="cta-button">Shop Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- user/src/Design/Pages/Home.js
+++ user/src/Design/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../Layouts/NavBar";
+import HeroSection from "../Layouts/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <HeroSection/>
     </div>
   );
 }
